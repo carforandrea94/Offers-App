9a3d4d0d11813f039f0b8f4967b87b434ea1ad83
carforandrea94 <carforandrea94.ac@gmail.com>
2017-08-31 09:37:18 +0200

    Salseman
    Correct positions of some tags

--- salsemanApp/www/js/controllers.js
+++ salsemanApp/www/js/controllers.js
@@ -101,107 +101,113 @@ angular.module('salseManApp.controllers', [])
 				quality: 80
 			};
 
-<<<<<<< HEAD
-=======
-		$ionicPlatform.ready(function() {
+			<<
+			<< << < HEAD
+				===
+				=== =
+				$ionicPlatform.ready(function() {
 
 
-		//method to set a photo from gallery
-		var galleryOpt = {
-			maximumImagesCount: 10,
-			width: 100,
-			height: 100,
-			quality: 80
-		};
->>>>>>> origin/master
+						//method to set a photo from gallery
+						var galleryOpt = {
+							maximumImagesCount: 10,
+							width: 100,
+							height: 100,
+							quality: 80
+						}; >>>
+						>>> > origin / master
 
-			$cordovaImagePicker.getPictures(galleryOpt)
-				.then(function(results) {
-					for(var i = 0; i < results.lenght; i++){
-						console.log('Image URI: ' + results[i]);
-						$scope.registrationData.imgSrc = results[i];
-					}
-				}, function(error) {
-					console.log(error);
-				});
-
-
-		};
-<<<<<<< HEAD
+						$cordovaImagePicker.getPictures(galleryOpt)
+							.then(function(results) {
+								for (var i = 0; i < results.lenght; i++) {
+									console.log('Image URI: ' + results[i]);
+									$scope.registrationData.imgSrc = results[i];
+								}
+							}, function(error) {
+								console.log(error);
+							});
 
 
-=======
-	});
->>>>>>> origin/master
+					}; <<
+					<< << < HEAD
+
+
+					===
+					=== =
+				}); >>>
+		>>> > origin / master
 	})
 
 	.controller('PublicationsCtrl', function($scope) {
-		$scope.publications = [{
-				title: 'Reggae',
-				id: 1,
-				category: "Normals"
-			},
-			{
-				title: 'Chill',
-				id: 2,
-				category: "Normals"
-			},
-			{
-				title: 'Dubstep',
-				id: 3,
-				category: 'Normals'
-			},
-			{
-				title: 'Indie',
-				id: 4,
-				category: 'Specials'
-			},
-			{
-				title: 'Rap',
-				id: 5,
-				category: 'Specials'
-			},
-			{
-				title: 'Cowbell',
-				id: 6,
-				category: 'Normals'
-			}
-		];
+			$scope.publications = [{
+						title: 'Publication Title',
+						description: 'Some Description',
+						special: false,
+						image: 'img/cioco.jpg'
+					},
+					{
+						title: 'Publication Title',
+						description: 'Some Description',
+						special: false,
+						image: 'img/cioco.jpg'
+					},
+					{
+						{
+							title: 'Publication Title',
+							description: 'Some Description',
+							special: false,
+							image: 'img/cioco.jpg'
+						},
+						{
+							title: 'Publication Title',
+							description: 'Some Description',
+							special: false,
+							image: 'img/cioco.jpg'
+						},
+						{
+							title: 'Publication Title',
+							description: 'Some Description',
+							special: false,
+							image: 'img/cioco.jpg' {
+								title: 'Publication Title',
+								description: 'Some Description',
+								special: false,
+								image: 'img/cioco.jpg'
+							}
+						];
 
-		$scope.select = function(setTab) {
-							 $scope.tab = setTab;
+						$scope.select = function(setTab) {
+							$scope.tab = setTab;
 
-							 if (setTab === 2) {
-									 $scope.filtText = "Normals";
-							 }
-							 else if (setTab === 3) {
-									 $scope.filtText = "Specials";
-							 }
-							 else {
-									 $scope.filtText = "";
-							 }
-					 };
+							if (setTab === 2) {
+								$scope.filtText = "Normals";
+							} else if (setTab === 3) {
+								$scope.filtText = "Specials";
+							} else {
+								$scope.filtText = "";
+							}
+						};
 
-					 $scope.isSelected = function (checkTab) {
-							 return ($scope.tab === checkTab);
-					 };
-	})
+						$scope.isSelected = function(checkTab) {
+							return ($scope.tab === checkTab);
+						};
+					})
 
-	.controller('PublicationDitailsCtrl', function($scope, $stateParams) {})
+				.controller('PublicationDitailsCtrl', function($scope, $stateParams) {})
 
-	.controller('ClientsCtrl', function($scope, $stateParams) {})
+				.controller('ClientsCtrl', function($scope, $stateParams) {})
 
-	.controller('ClientDitailsCtrl', function($scope, $stateParams) {})
+				.controller('ClientDitailsCtrl', function($scope, $stateParams) {})
 
-	.controller('AccountCtrl', function($scope, $stateParams) {})
+				.controller('AccountCtrl', function($scope, $stateParams) {})
 
-	.controller('MessagesCtrl', function($scope, $stateParams) {
-		$scope.expandText = function(){
-			var element = document.getElementById("messageArea");
-			element.style.height =  element.scrollHeight + "px";
+				.controller('MessagesCtrl', function($scope, $stateParams) {
+					$scope.expandText = function() {
+						var element = document.getElementById("messageArea");
+						element.style.height = element.scrollHeight + "px";
 
-			$scope.clearMessage = function() {
-				var element = document.getElementById("messageArea").value = "";
-			}
-		}
-	});
+						$scope.clearMessage = function() {
+							var element = document.getElementById("messageArea").value = "";
+						}
+					}
+				});
